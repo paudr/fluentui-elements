dc2910a47b7e2daaaad55d3093b41439b5e8b1e9
Pau <frikeldon@gmail.com>
2020-08-08 19:39:43 +0200

    Clase base ComboElement

---
+++ src/base/combo-element/combo-element.css.js
@@ -0,0 +1,205 @@
+import { css } from 'lit-element'
+
+import { fontStyle, fontSize } from '../../theme/typografy.css'
+import { slideDownIn20 } from '../../theme/animation.css'
+import { normalize, getFocusStyle, getLabelStyle } from '../../theme/mixins.css'
+import {
+  blackTranslucent40,
+  neutralLight,
+  neutralLighter,
+  neutralPrimary,
+  neutralSecondary,
+  redDark,
+  themePrimary,
+  white
+} from '../../theme/color.css'
+
+const backgroundDisabledColor = neutralLighter
+
+const borderColor = neutralSecondary
+const borderHoverColor = neutralPrimary
+const borderFocusColor = themePrimary
+const borderInvalidColor = redDark
+
+export default css`
+  :host {
+    display: block;
+  }
+
+  #root {
+    ${normalize}
+    ${fontStyle.medium};
+    position: relative;
+  }
+
+  ${getLabelStyle('#label')}
+
+  #fieldGroup {
+    ${normalize};
+    border: 0px solid transparent;
+    border-radius: 2px;
+    background: ${white};
+    height: 32px;
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    cursor: text;
+  }
+
+  :host([disabled]) #fieldGroup {
+    background-color: ${backgroundDisabledColor};
+    cursor: auto;
+  }
+
+  ${getFocusStyle(
+    ':host(:not([disabled]):not([underlined]):not([borderless])) #fieldGroup',
+    {
+      color: borderColor,
+      position: css`0px`,
+      width: css`1px`,
+      radius: css`2px`
+    }
+  )}
+
+  ${getFocusStyle(
+    ':host(:not([disabled]):not([underlined]):not([borderless])) #fieldGroup:hover',
+    {
+      color: borderHoverColor,
+      position: css`0px`,
+      width: css`1px`,
+      radius: css`2px`
+    }
+  )}
+
+  ${getFocusStyle(
+    ':host(:not([disabled]):not([underlined]):not([borderless]):not([open])) #fieldGroup:focus-within',
+    {
+      color: borderFocusColor,
+      radius: css`2px`
+    }
+  )}
+
+  ${getFocusStyle(
+    ':host(:not([disabled]):not([underlined]):not([borderless])) #root.invalid #fieldGroup',
+    {
+      color: borderInvalidColor,
+      position: css`0px`,
+      width: css`1px`,
+      radius: css`2px`
+    }
+  )}
+
+  ${getFocusStyle(
+    ':host(:not([disabled]):not([underlined]):not([borderless]):not([open])) #root.invalid #fieldGroup:focus-within',
+    {
+      color: borderInvalidColor,
+      radius: css`2px`
+    }
+  )}
+
+  #description {
+    color: ${neutralSecondary};
+    ${fontSize.xSmall};
+  }
+
+  #errorMessage {
+    ${fontSize.small};
+    color: ${redDark};
+    margin: 0;
+    padding-top: 5px;
+    display: flex;
+    align-items: center;
+  }
+
+  :host(:not([underlined])) #root.requiredPlaceholder #wrapper::after {
+    content: '*';
+    color: ${redDark};
+    position: absolute;
+    top: -5px;
+    right: -10px;
+  }
+
+  /* Underlined */
+
+  :host([underlined]) #wrapper {
+    text-overflow: ellipsis;
+    position: relative;
+    display: flex;
+    width: 100%;
+  }
+  
+  :host([underlined]) #label {
+    margin-right: 8px;
+    padding-left: 12px;
+    line-height: 22px;
+    height: 32px;
+  }
+  
+  :host([underlined]) #fieldWrapper {
+    flex: 1 1 0px;
+    border-width: 0;
+    text-align: left;
+  }
+
+  :host([underlined]) #root.requiredPlaceholder #fieldGroup::after {
+    content: '*';
+    color: ${redDark};
+    position: absolute;
+    top: -5px;
+    right: -10px;
+  }
+
+  ${getFocusStyle(':host([underlined]) #wrapper', {
+    color: borderColor,
+    width: css`1px`,
+    radius: css`0px`,
+    type: 'borderBottom'
+  })}
+
+  ${getFocusStyle(':host([underlined]:not([open])) #wrapper:focus-within', {
+    color: borderFocusColor,
+    radius: css`0px`,
+    type: 'borderBottom'
+  })}
+
+  ${getFocusStyle(':host([underlined]) #root.invalid #wrapper', {
+    color: borderInvalidColor,
+    width: css`1px`,
+    radius: css`0px`,
+    type: 'borderBottom'
+  })}
+
+  ${getFocusStyle(
+    ':host([underlined]:not([open])) #root.invalid #wrapper:focus-within',
+    {
+      color: borderInvalidColor,
+      radius: css`0px`,
+      type: 'borderBottom'
+    }
+  )}
+
+  ${getFocusStyle(':host([underlined][disabled]) #wrapper', {
+    color: backgroundDisabledColor,
+    width: css`1px`,
+    radius: css`0px`,
+    type: 'borderBottom'
+  })}
+
+  #dropdown {
+    ${normalize};
+    background-color: ${white};
+    display: none;
+    position: absolute;
+    width: 100%;
+    z-index: 400;
+    box-shadow: 0 0px 15px -5px ${blackTranslucent40};
+    border: 1px solid ${neutralLight};
+    user-select: none;
+  }
+
+  :host([open]) #dropdown {
+    display: block;
+  }
+
+  ${slideDownIn20}
+`
---
+++ src/base/combo-element/combo-element.js
@@ -0,0 +1,99 @@
+import { html, nothing } from 'lit-html'
+import { classMap } from 'lit-html/directives/class-map'
+import StyledElement from '../styled-element'
+import styles from './combo-element.css'
+
+export default class ComboElement extends StyledElement {
+  static get styles () {
+    return styles
+  }
+
+  static get properties () {
+    return {
+      label: { type: String, reflect: true },
+      description: { type: String, reflect: true },
+      invalid: { type: Boolean, reflect: true },
+      errorMessage: { type: String, reflect: true },
+      disabled: { type: Boolean, reflect: true },
+      required: { type: Boolean, reflect: true },
+      borderless: { type: Boolean, reflect: true },
+      underlined: { type: Boolean, reflect: true },
+      open: { type: Boolean, reflect: true }
+    }
+  }
+
+  constructor () {
+    super()
+
+    this.label = ''
+    this.description = ''
+    this.errorMessage = ''
+    this.invalid = false
+    this.disabled = false
+    this.required = false
+    this.borderless = false
+    this.underlined = false
+  }
+
+  labelClick () {}
+
+  renderLabel () {
+    return !this.label
+      ? nothing
+      : this.constructor.fieldId
+        ? html`<label id="label" for="${this.constructor.fieldId}">
+          ${this.label}
+        </label>`
+        : html`<label id="label" @click="${this.labelClick}">
+          ${this.label}
+        </label>`
+  }
+
+  renderInputField () {
+    return nothing
+  }
+
+  renderDropdown () {
+    return nothing
+  }
+
+  render () {
+    return html`
+      <div
+        id="root"
+        class="${classMap({
+          requiredPlaceholder: !this.label && this.required,
+          invalid: this.invalid || this.errorMessage
+        })}"
+      >
+        <div id="wrapper">
+          ${this.renderLabel()}
+          <div id="fieldWrapper">
+            <div id="fieldGroup">
+              ${this.renderInputField()}
+            </div>
+            <div id="dropdown">
+              ${this.renderDropdown()}
+            </div>
+          </div>
+        </div>
+        ${this.description || this.errorMessage
+          ? html`
+              <span>
+                ${this.description
+                  ? html`<span id="description">${this.description}</span>`
+                  : nothing}
+                ${this.errorMessage
+                  ? html`
+                      <p id="errorMessage" class="slideDownIn20">
+                        <span>${this.errorMessage}</span>
+                      </p>
+                    `
+                  : nothing}
+              </span>
+            `
+          : nothing}
+      </div>
+    `
+  }
+}
