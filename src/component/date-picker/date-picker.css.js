403cdc0f984117eba5fd32e28f6f16175675f7ba
Pau <frikeldon@gmail.com>
2020-08-11 12:48:46 +0200

    Se corrigen los estilos de DatePicker

--- src/component/date-picker/date-picker.css.js
+++ src/component/date-picker/date-picker.css.js
@@ -9,13 +9,12 @@ export default css`
     ${fontStyle.medium}
     ${normalize}
     padding: 0px 24px 0px 8px;
+    flex: 1 1 0px;
     color: ${neutralPrimary};
-    width: 100%;
-    min-width: 0px;
     text-overflow: ellipsis;
-    border-width: 0px;
+    border: 0;
+    outline: 0;
     background: none transparent;
-    outline: 0px;
   }
 
   i {
@@ -29,10 +28,7 @@ export default css`
     cursor: pointer;
   }
 
-  #calendar {
-    display: block;
-    position: absolute;
-    z-index: 400;
+  fluent-calendar {
     box-shadow: rgba(0, 0, 0, 0.133) 0px 3.2px 7.2px 0px, rgba(0, 0, 0, 0.11) 0px 0.6px 1.8px 0px;
   }
 `
