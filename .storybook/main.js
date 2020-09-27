907c146ba56b2760eece35a1616857a547dffc13
Pau <frikeldon@gmail.com>
2020-09-28 00:35:27 +0200

    Se instala el addon docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ const packageJson = require('../package.json')
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-controls',
     '@storybook/addon-storysource'
