0c735ad382515747bc196fee4d15a565608c2279
Pau <frikeldon@gmail.com>
2020-09-28 13:11:34 +0200

    Se actualizan las historias de Checkbox

--- src/component/choice-group/arg-types.js
+++ src/component/choice-group/arg-types.js
@@ -3,48 +3,86 @@ export default {
     name: 'value',
     type: { name: 'string', required: false },
     defaultValue: '',
+    description: 'Current value of the choice group.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' }
     }
   },
   label: {
     name: 'label',
     type: { name: 'string', required: false },
     defaultValue: '',
+    description: 'Descriptive label for the choice group.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' }
     }
   },
   required: {
     name: 'required',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description:
+      'If true, it specifies that an option must be selected in the choice group before submitting the form.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   disabled: {
     name: 'disabled',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description: 'Whether the choice group is disabled or not.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   options: {
     name: 'options',
     type: { name: 'object', required: false },
     defaultValue: [],
+    description: 'The options for the choice group.',
     control: {
       type: 'object'
+    },
+    table: {
+      type: {
+        summary: '[...objects]',
+        detail: `[...{
+  value (string),
+  text (string),
+  disabled (boolean)
+}]`
+      },
+      defaultValue: { summary: '[]' }
     }
   },
   inRow: {
     name: 'inRow',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description: 'Render the choice group in a row instead of a column.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   }
 }
--- src/component/choice-group/choice-group.stories.js
+++ src/component/choice-group/choice-group.stories.js
@@ -2,54 +2,29 @@ import { action } from '@storybook/addon-actions'
 import argTypes from './arg-types'
 import './choice-group'
 
-function renderChoiceGroup (args) {
-  const choiceGroup = document.createElement('FLUENT-CHOICE-GROUP')
-
-  choiceGroup.addEventListener('change', action('change'))
-
-  for (const prop in args) {
-    choiceGroup[prop] = args[prop]
-  }
-
-  return choiceGroup
-}
-
-const choiceOptions = [
-  { value: 'A', text: 'Option A' },
-  { value: 'B', text: 'Option B' },
-  { value: 'C', text: 'Option C', disabled: true },
-  { value: 'D', text: 'Option D' }
-]
-
 export default {
   title: 'Basic Inputs/ChoiceGroup',
   component: 'fluent-choice-group',
   argTypes
 }
 
-export const Normal = renderChoiceGroup.bind({})
-Normal.args = {
-  label: 'Pick one',
-  options: choiceOptions
-}
+export function Standard (args) {
+  const choiceGroup = document.createElement('FLUENT-CHOICE-GROUP')
 
-export const Required = renderChoiceGroup.bind({})
-Required.args = {
-  label: 'Pick one',
-  options: choiceOptions,
-  required: true
-}
+  for (const prop in args) {
+    choiceGroup[prop] = args[prop]
+  }
 
-export const Disabled = renderChoiceGroup.bind({})
-Disabled.args = {
-  label: 'Pick one',
-  options: choiceOptions,
-  disabled: true
-}
+  choiceGroup.addEventListener('change', action('change'))
 
-export const InRow = renderChoiceGroup.bind({})
-InRow.args = {
+  return choiceGroup
+}
+Standard.args = {
   label: 'Pick one',
-  options: choiceOptions,
-  InRow: true
+  options: [
+    { value: 'A', text: 'Option A' },
+    { value: 'B', text: 'Option B' },
+    { value: 'C', text: 'Option C', disabled: true },
+    { value: 'D', text: 'Option D' }
+  ]
 }
