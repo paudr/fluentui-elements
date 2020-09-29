35157d6cf109131676f96cbe191f8f870b09a91f
Pau <frikeldon@gmail.com>
2020-09-29 14:15:19 +0200

    Se actualizan las historias de Label

--- src/component/label/arg-types.js
+++ src/component/label/arg-types.js
@@ -3,24 +3,39 @@ export default {
     name: 'textContent',
     type: { name: 'string', required: false },
     defaultValue: '',
+    description: 'The text content of the DOM Element',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' }
     }
   },
   disabled: {
     name: 'disabled',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description: 'Renders the label as disabled.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   required: {
     name: 'required',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description: 'Whether the associated form field is required or not.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   }
 }
--- src/component/label/label.stories.js
+++ src/component/label/label.stories.js
@@ -1,7 +1,13 @@
 import argTypes from './arg-types'
 import './label'
 
-function renderLabel (args) {
+export default {
+  title: 'Basic Inputs/Label',
+  component: 'fluent-label',
+  argTypes
+}
+
+export function Standard (args) {
   const label = document.createElement('FLUENT-LABEL')
 
   for (const prop in args) {
@@ -10,33 +16,6 @@ function renderLabel (args) {
 
   return label
 }
-
-export default {
-  title: 'Basic Inputs/Label',
-  component: 'fluent-label',
-  argTypes
-}
-
-export const Normal = renderLabel.bind({})
-Normal.args = {
+Standard.args = {
   textContent: "I'm a Label"
-}
-
-export const Required = renderLabel.bind({})
-Required.args = {
-  textContent: "I'm a required Label",
-  required: true
-}
-
-export const Disabled = renderLabel.bind({})
-Disabled.args = {
-  textContent: "I'm a disabled Label",
-  disabled: true
-}
-
-export const RequiredDisabled = renderLabel.bind({})
-RequiredDisabled.args = {
-  textContent: "I'm a required and disabled Label",
-  required: true,
-  disabled: true
 }
