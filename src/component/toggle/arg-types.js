287b8e9ac49b51c2ba6051e6f8f929d1faac7b42
Pau <frikeldon@gmail.com>
2020-09-30 00:37:04 +0200

    Se actualizan las historias de Toggle

--- src/component/toggle/arg-types.js
+++ src/component/toggle/arg-types.js
@@ -3,48 +3,81 @@ export default {
     name: 'checked',
     type: { name: 'boolean', required: false },
     defaultValue: '',
+    description: 'Checked state of the toggle.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   label: {
     name: 'label',
     type: { name: 'string', required: false },
     defaultValue: '',
+    description: 'A label for the toggle.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' }
     }
   },
   onText: {
     name: 'onText',
     type: { name: 'string', required: false },
     defaultValue: '',
+    description:
+      'Text to display when toggle is ON. Caution: when not providing on/off text user may get confused in differentiating the on/off states of the toggle.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' }
     }
   },
   offText: {
     name: 'offText',
     type: { name: 'string', required: false },
     defaultValue: '',
+    description:
+      'Text to display when toggle is OFF. Caution: when not providing on/off text user may get confused in differentiating the on/off states of the toggle.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' }
     }
   },
   inline: {
     name: 'inline',
     type: { name: 'boolean', required: false },
     defaultValue: '',
+    description:
+      'Whether the label (not the onText/offText) should be positioned inline with the toggle control.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   disabled: {
     name: 'disabled',
     type: { name: 'boolean', required: false },
     defaultValue: '',
+    description: 'Optional disabled flag.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   }
 }
--- src/component/toggle/toggle.stories.js
+++ src/component/toggle/toggle.stories.js
@@ -2,7 +2,13 @@ import { action } from '@storybook/addon-actions'
 import argTypes from './arg-types'
 import './toggle'
 
-function renderToggle (args) {
+export default {
+  title: 'Basic Inputs/Toggle',
+  component: 'fluent-toggle',
+  argTypes
+}
+
+export function Standard (args) {
   const toggle = document.createElement('FLUENT-TOGGLE')
   toggle.addEventListener('keydown', event => event.stopPropagation())
 
@@ -14,78 +20,9 @@ function renderToggle (args) {
 
   return toggle
 }
-
-export default {
-  title: 'Basic Inputs/Toggle',
-  component: 'fluent-toggle',
-  argTypes
-}
-
-export const EnabledAndChecked = renderToggle.bind({})
-EnabledAndChecked.args = {
+Standard.args = {
   checked: true,
-  label: 'Enabled and checked',
+  label: 'Standard',
   onText: 'On',
   offText: 'Off'
-}
-
-export const EnabledAndUnchecked = renderToggle.bind({})
-EnabledAndUnchecked.args = {
-  label: 'Enabled and unchecked',
-  onText: 'On',
-  offText: 'Off'
-}
-
-export const DisableAndChecked = renderToggle.bind({})
-DisableAndChecked.args = {
-  checked: true,
-  label: 'Disable and checked',
-  onText: 'On',
-  offText: 'Off',
-  disabled: true
-}
-
-export const DisableAndUnchecked = renderToggle.bind({})
-DisableAndUnchecked.args = {
-  label: 'Disable and unchecked',
-  onText: 'On',
-  offText: 'Off',
-  disabled: true
-}
-
-export const WithoutText = renderToggle.bind({})
-WithoutText.args = {
-  label: 'Without text'
-}
-
-export const WithInlineLabel = renderToggle.bind({})
-WithInlineLabel.args = {
-  label: 'With inline label',
-  onText: 'On',
-  offText: 'Off',
-  inline: true
-}
-
-export const DisabledWithInlineLabel = renderToggle.bind({})
-DisabledWithInlineLabel.args = {
-  label: 'Disabled with inline label',
-  onText: 'On',
-  offText: 'Off',
-  inline: true,
-  disabled: true
-}
-
-export const WithInlineLabelAndWithoutOnTextAndOffText = renderToggle.bind({})
-WithInlineLabelAndWithoutOnTextAndOffText.args = {
-  label: 'With inline label and without onText and offText',
-  inline: true
-}
-
-export const DisabledWithInlineLabelAndWithoutOnTextAndOffText = renderToggle.bind(
-  {}
-)
-DisabledWithInlineLabelAndWithoutOnTextAndOffText.args = {
-  label: 'With inline label and without onText and offText',
-  disabled: true,
-  inline: true
 }
