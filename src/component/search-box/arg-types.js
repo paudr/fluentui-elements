983e226419efaaf5a1358084b34ba9213aa0fc89
Pau <frikeldon@gmail.com>
2020-09-29 18:05:06 +0200

    Se actualizan las historias de SearchBox

--- src/component/search-box/arg-types.js
+++ src/component/search-box/arg-types.js
@@ -6,34 +6,54 @@ export default {
   icon: {
     name: 'icon',
     type: { name: 'enum', required: false },
-    defaultValue: null,
+    defaultValue: 'Search',
+    description: 'The name of the icon to use from the icon font.',
     control: {
       type: 'select',
       options: ['', ...Object.keys(iconCode)]
+    },
+    table: {
+      type: { summary: 'string (iconName)' },
+      defaultValue: { summary: 'Search' }
     }
   },
   placeholder: {
     name: 'placeholder',
     type: { name: 'string', required: false },
-    defaultValue: '',
+    defaultValue: 'Search',
+    description: 'Placeholder for the search box.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: 'Search' }
     }
   },
   disableAnimation: {
     name: 'disableAnimation',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description: 'Whether or not to animate the SearchBox icon on focus.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   value: {
     name: 'value',
     type: { name: 'string', required: false },
     defaultValue: '',
+    description: 'The value of the text in the SearchBox.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: 'Search' }
     }
   }
 }
--- src/component/search-box/search-box.basic.stories.js
+++ src/component/search-box/search-box.basic.stories.js
@@ -2,7 +2,13 @@ import { action } from '@storybook/addon-actions'
 import argTypes from './arg-types'
 import './search-box'
 
-function renderSearchBox (args) {
+export default {
+  title: 'Basic Inputs/SearchBox',
+  component: 'fluent-search-box',
+  argTypes
+}
+
+export function Standard (args) {
   const searchBox = document.createElement('FLUENT-SEARCH-BOX')
   searchBox.addEventListener('keydown', event => event.stopPropagation())
 
@@ -16,64 +22,4 @@ function renderSearchBox (args) {
   }
 
   return searchBox
-}
-
-export default {
-  title: 'Basic Inputs/SearchBox/Basic',
-  component: 'fluent-search-box',
-  argTypes
-}
-
-export const Normal = renderSearchBox.bind({})
-
-export const WithLabel = renderSearchBox.bind({})
-WithLabel.args = {
-  label: 'Query string:'
-}
-
-export const WithDescription = renderSearchBox.bind({})
-WithDescription.args = {
-  description: 'A fancy description.'
-}
-
-export const Invalid = renderSearchBox.bind({})
-Invalid.args = {
-  Invalid: true
-}
-
-export const WithErrorMessage = renderSearchBox.bind({})
-WithErrorMessage.args = {
-  errorMessage: 'Error message'
-}
-
-export const Disabled = renderSearchBox.bind({})
-Disabled.args = {
-  disabled: true
-}
-
-export const Required = renderSearchBox.bind({})
-Required.args = {
-  label: 'Required',
-  required: true
-}
-
-export const RequiredWithoutLabel = renderSearchBox.bind({})
-RequiredWithoutLabel.args = {
-  required: true
-}
-
-export const NoAnimations = renderSearchBox.bind({})
-NoAnimations.args = {
-  disableAnimation: true
-}
-
-export const CustomIcon = renderSearchBox.bind({})
-CustomIcon.args = {
-  icon: 'Filter',
-  placeholder: 'Filter'
-}
-
-export const Borderless = renderSearchBox.bind({})
-Borderless.args = {
-  borderless: true
 }
