a8358697451c08d50679a5543911c6f3545c55cf
Pau <frikeldon@gmail.com>
2020-09-30 13:29:53 +0200

    Se actualizan las historias de Pivot

--- src/component/pivot/arg-types.js
+++ src/component/pivot/arg-types.js
@@ -3,41 +3,72 @@ export default {
     name: 'selectedTab',
     type: { name: 'number', required: false },
     defaultValue: -1,
+    description: 'Index of the selected pivot item.',
     control: {
       type: 'number',
       step: 1
+    },
+    table: {
+      type: { summary: 'number' },
+      defaultValue: { summary: -1 }
     }
   },
   tabs: {
     name: 'tabs',
-    type: { name: 'array', required: false },
-    defaultValue: '',
+    type: { name: 'object', required: false },
+    defaultValue: [],
+    description: 'Tabs to render.',
     control: {
       type: 'object'
+    },
+    table: {
+      type: {
+        summary: '[...objects]',
+        detail: `[...{
+  text (string),
+  value (string)
+}]`
+      },
+      defaultValue: { summary: '[]' }
     }
   },
   large: {
     name: 'large',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description: 'Whether tabs are rendered large or not.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   tabStyle: {
     name: 'tabStyle',
     type: { name: 'boolean', required: false },
     defaultValue: false,
+    description: 'Whether tabs are rendered as tabs or links.',
     control: {
       type: 'boolean'
+    },
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' }
     }
   },
   value: {
     name: 'value',
     type: { name: 'string', required: false },
     defaultValue: null,
+    description: 'Value of the selected tab.',
     control: {
       type: 'text'
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' }
     }
   }
 }
--- src/component/pivot/pivot.stories.js
+++ src/component/pivot/pivot.stories.js
@@ -2,7 +2,13 @@ import { action } from '@storybook/addon-actions'
 import argTypes from './arg-types'
 import './pivot'
 
-function renderPivot (args) {
+export default {
+  title: 'Commands, Menus & Navs/Pivot',
+  component: 'fluent-pivot',
+  argTypes
+}
+
+export function Standard (args) {
   const pivot = document.createElement('FLUENT-PIVOT')
 
   for (const prop in args) {
@@ -13,60 +19,10 @@ function renderPivot (args) {
 
   return pivot
 }
-
-const fullTabs = [
-  { text: 'My files', value: 'files', icon: 'Emoji2', count: 42 },
-  { text: 'Recent', value: 'recent', icon: 'Recent', count: 23 },
-  { text: 'Shared with me', value: 'shared', icon: 'Ringer', count: 1 }
-]
-
-const onlyTextTabs = fullTabs.map(tab => ({
-  text: tab.text,
-  value: tab.value
-}))
-
-const withIconsTabs = fullTabs.map(tab => ({
-  text: tab.text,
-  value: tab.value,
-  icon: tab.icon
-}))
-
-export default {
-  title: 'Commands, Menus & Navs/Pivot',
-  component: 'fluent-pivot',
-  argTypes
-}
-
-export const OnlyText = renderPivot.bind({})
-OnlyText.args = {
-  tabs: onlyTextTabs
-}
-
-export const WithIcons = renderPivot.bind({})
-WithIcons.args = {
-  tabs: withIconsTabs
-}
-
-export const FullTabs = renderPivot.bind({})
-FullTabs.args = {
-  tabs: fullTabs
-}
-
-export const Large = renderPivot.bind({})
-Large.args = {
-  tabs: onlyTextTabs,
-  large: true
-}
-
-export const TabStyle = renderPivot.bind({})
-TabStyle.args = {
-  tabs: onlyTextTabs,
-  tabStyle: true
-}
-
-export const LargeTabStyle = renderPivot.bind({})
-LargeTabStyle.args = {
-  tabs: onlyTextTabs,
-  large: true,
-  tabStyle: true
+Standard.args = {
+  tabs: [
+    { text: 'My files', value: 'files', icon: 'Emoji2', count: 42 },
+    { text: 'Recent', value: 'recent', icon: 'Recent', count: 23 },
+    { text: 'Shared with me', value: 'shared', icon: 'Ringer', count: 1 }
+  ]
 }
